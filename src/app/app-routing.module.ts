260faf5858ba83fcae0e2df2349970a2d3c2381c
EWD <support@ewdtech.comGithub>
2021-11-10 11:26:36 +0500

    ewd-tech

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './Components/home/home.component';
 import {ServicesComponent} from './Components/services/services.component';
 import {ECommerceComponent} from './Components/e-commerce/e-commerce.component';
-import {AboutusComponent} from './Components/aboutus/aboutus.component';
+import {PortfolioComponent} from './Components/portfolio/portfolio.component';
 import {BlogComponent} from './Components/blog/blog.component';
 import {ContactComponent} from './Components/contact/contact.component';
-import {PortfolioComponent} from './Components/portfolio/portfolio.component';
+import {AboutusComponent} from './Components/aboutus/aboutus.component';
+
+
 
 
 const routes: Routes = [
@@ -17,7 +19,6 @@ const routes: Routes = [
   { path: 'blog', component: BlogComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'aboutus', component: AboutusComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
   { path: '**', component: HomeComponent } // Wildcard route for a 404 page
 ];
 
